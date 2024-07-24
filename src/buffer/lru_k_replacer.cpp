04a4c2c446a641c77e80bc5e2f60c93c59de5a62
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-24 12:30:15 +0800

    feat: modify the ds for LRUKReplacer and add evict method

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -11,13 +11,79 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/lru_k_replacer.h"
+#include <mutex>
 #include "common/exception.h"
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {
+    // Check if k and num_frames are valid
+    if (k_ < 1) {
+        throw Exception(ExceptionType::INVALID, "k must be greater than 0");
+    }
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+    if (replacer_size_ < 0) {
+        throw Exception(ExceptionType::INVALID, "replacer size must be greater than 0");
+    }
+
+    // Initialize the head and tail nodes
+    less_than_k_head_ = std::make_shared<LRUKNode>();
+    less_than_k_tail_ = std::make_shared<LRUKNode>();
+
+    // Set the pointers of the head and tail nodes
+    less_than_k_head_->next_ = less_than_k_tail_;
+    less_than_k_tail_->prev_ = less_than_k_head_;
+
+}
+
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+    // Lock the latch
+    std::lock_guard<std::mutex> lock(latch_);
+
+    // Check if the replacer is empty
+    if(replacer_size_ == 0) {
+        latch_.unlock();
+        return false;
+    }
+
+    // Find the frame with the maximum backward k-distance
+    // If there exists +inf, return it
+    if(less_than_k_head_->next_ != less_than_k_tail_) {
+        // Get the frame id
+        *frame_id = less_than_k_head_->next_->fid_;
+
+        // Remove the frame from the list
+        less_than_k_head_->next_->next_->prev_ = less_than_k_head_;
+        less_than_k_head_->next_ = less_than_k_head_->next_->next_;
+
+        // Decrement the size of the replacer
+        replacer_size_--;
+
+        // Unlock the latch
+        latch_.unlock();
+        return true;
+    }
+
+    // If there is no +inf, return the frame with the maximum backward k-distance
+    if(!greater_than_k_set_.empty()) {
+        // Get the frame id
+        *frame_id = (*greater_than_k_set_.begin())->fid_;
+
+        // Remove the frame from the set
+        greater_than_k_set_.erase(greater_than_k_set_.begin());
+
+        // Decrement the size of the replacer
+        replacer_size_--;
+
+        // Unlock the latch
+        latch_.unlock();
+        return true;
+    }
+
+    // Unlock the latch
+    latch_.unlock();
+    return false;
+}
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
 
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -14,7 +14,10 @@
 
 #include <limits>
 #include <list>
+#include <set>
+#include <memory>
 #include <mutex>  // NOLINT
+#include <shared_mutex>
 #include <unordered_map>
 #include <vector>
 
@@ -26,14 +29,27 @@ namespace bustub {
 enum class AccessType { Unknown = 0, Lookup, Scan, Index };
 
 class LRUKNode {
- private:
+ public:
   /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
   // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
 
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
+  std::list<size_t> history_; 
+  size_t k_;
+  frame_id_t fid_;
+  bool is_evictable_{false};
+
+  // Pointers to the previous and next nodes in the list
+  std::shared_ptr<LRUKNode> prev_{nullptr};
+  std::shared_ptr<LRUKNode> next_{nullptr};
+
+  LRUKNode() = default;
+};
+
+// Custom comparator for the set
+struct MyCompare {
+  auto operator()(const std::shared_ptr<LRUKNode> &lhs, const std::shared_ptr<LRUKNode> &rhs) const -> bool {
+    return lhs->history_.front() < rhs->history_.front();
+  }
 };
 
 /**
@@ -150,12 +166,22 @@ class LRUKReplacer {
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  std::unordered_map<frame_id_t, LRUKNode> node_store_;
+
+  // Liked list for nodes with less than k history
+  std::shared_ptr<LRUKNode> less_than_k_head_;
+  std::shared_ptr<LRUKNode> less_than_k_tail_;
+
+  // RB-Tree for nodes with greater than k history
+  std::set<std::shared_ptr<LRUKNode>, MyCompare> greater_than_k_set_;
+
+  size_t current_timestamp_{0};
+  size_t curr_size_{0};
+
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
 };
+
 
 }  // namespace bustub
