43617189373d2cf71c40f5afaf5a8975eee97fd6
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-07-30 22:37:48 +0800

    feat: finish FetchPage

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -96,7 +96,70 @@ auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
 }
 
 auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType access_type) -> Page * {
-  return nullptr;
+  // Lock the latch
+  std::lock_guard<std::mutex> lock(latch_);
+
+  // Check if the page is in the buffer pool
+  if (page_table_.find(page_id) != page_table_.end()) {
+    // If the page is in the buffer pool, we can return it
+    frame_id_t frame_id = page_table_[page_id];
+    replacer_ -> RecordAccess(frame_id);
+    replacer_ -> SetEvictable(frame_id, false);
+    pages_[frame_id].pin_count_++;
+    return &pages_[frame_id];
+  }      
+    
+  // If the page is not in the buffer pool, we need to fetch it from the disk
+  frame_id_t empty_frame_id{0};
+
+  // Firstly check if there is any free frame
+  if (free_list_.empty()) {
+    // If there is no free frame, we need to evict a page
+    if (!replacer_->Evict(&empty_frame_id)) {
+      return nullptr;
+    }
+
+    // Get the page_id of the page in the frame
+    auto prev_page_id = pages_[empty_frame_id].GetPageId();
+    page_table_.erase(prev_page_id);
+
+    // If the page is dirty, we need to write it back to the disk
+    if (pages_[empty_frame_id].IsDirty()) {
+      // Wrap the page in a DiskRequest
+      DiskRequest request{.is_write_ = true, .data_ = pages_[empty_frame_id].GetData(), .page_id_ = prev_page_id};
+
+      // Create a promise to signal the completion of the request
+      auto feedback{request.callback_.get_future()};
+      // Schedule the request
+      disk_scheduler_->Schedule(std::move(request));
+      // Wait for the request to complete
+      feedback.get();
+    }
+  } else {
+    // If there is a free frame, we can use it
+    empty_frame_id = free_list_.front();
+    free_list_.pop_front();
+  }
+
+  // Read the page from the disk
+  DiskRequest request{.is_write_ = false, .data_ = pages_[empty_frame_id].GetData(), .page_id_ = page_id};
+
+  // Create a promise to signal the completion of the request
+  auto feedback{request.callback_.get_future()};
+  // Schedule the request
+  disk_scheduler_->Schedule(std::move(request));
+  // Wait for the request to complete
+  feedback.get();
+
+  // Update the metadata for the new page
+  page_table_[page_id] = empty_frame_id;
+  SetPage(empty_frame_id, page_id);
+
+  // Pin the frame in replacer
+  replacer_ -> RecordAccess(empty_frame_id);
+  replacer_ -> SetEvictable(empty_frame_id, false);
+
+  return &pages_[empty_frame_id];
 }
 
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unused]] AccessType access_type) -> bool {
