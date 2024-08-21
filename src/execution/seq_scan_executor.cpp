5d180526420b7f436c0d083ca7f6581923bd8017
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-08-21 11:21:22 +0800

    feat: seq_scan_executoe

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,53 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <cstddef>
+#include <memory>
+#include "storage/table/table_iterator.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan), iter_(nullptr) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+    // Get the table info from the catalog
+    auto table_info = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+    schema_ = &table_info->schema_;
+    // Get the table iterator
+    iter_ = std::make_unique<TableIterator>(table_info->table_->MakeIterator());
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    for(; iter_->IsEnd(); ++(*iter_)){
+        // Get the tuple and the meta data
+        auto [meta, tup] = iter_->GetTuple();
+        
+        // Check if the tuple is deleted
+        if(meta.is_deleted_){
+            continue;
+        }
+
+        // Update the output value
+        *tuple = tup;
+        *rid = iter_->GetRID();
+
+        // Check if the tuple satisfies the predicate
+        if(plan_->filter_predicate_ != nullptr){
+            // Evaluate the predicate
+            auto eval = plan_->filter_predicate_->Evaluate(&tup, *schema_);
+            
+            if(!eval.GetAs<bool>()){
+                continue;
+            }
+        }
+
+        // Increment the iterator for next iteration
+        ++(*iter_);
+
+        return true;
+    }
+
+    return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -14,6 +14,7 @@
 
 #include <vector>
 
+#include "catalog/schema.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
@@ -50,5 +51,11 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+
+  // Iterator for the table to be scanned
+  std::unique_ptr<TableIterator> iter_;
+
+  // The schema of the table to be scanned for arithmetics
+  Schema *schema_;
 };
 }  // namespace bustub
