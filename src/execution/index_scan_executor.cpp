d4b67ed1b87ced61b5485dae889fd70286da45be
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-09-02 15:41:37 +0800

    feat: index_scan executor

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -13,10 +13,53 @@
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void IndexScanExecutor::Init() { throw NotImplementedException("IndexScanExecutor is not implemented"); }
+void IndexScanExecutor::Init() {
+    // Get the table info
+    auto catalog = exec_ctx_->GetCatalog();
+    table_info_ = catalog->GetTable(plan_->table_oid_);
 
-auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    // Get the table indexes
+    auto indexes = catalog->GetTableIndexes(table_info_->name_);
+
+    // Find the target index and convert it to HashTableIndexForTwoIntegerColumn
+    for(auto index : indexes) {
+        if(index->index_oid_ == plan_->index_oid_) {
+            htable_index_ = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(index->index_.get());
+            break;
+        }
+    }
+}
+
+auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    // Check if the table info and index are valid
+    if(table_info_ == nullptr || htable_index_ == nullptr) {
+        return false;
+    }
+
+    // Construct the key
+    std::vector<Value> value{plan_->pred_key_->val_};
+    Tuple key{value, htable_index_->GetKeySchema()};
+
+    // Scan the index to get the RIDs
+    std::vector<RID> rids;
+    htable_index_->ScanKey(key, &rids, exec_ctx_->GetTransaction());
+    if (rids.empty()) {
+        return false;
+    }
+
+    // Check if the RIDs are duplicated
+    BUSTUB_ASSERT(rids.size() == 1, "IndexScaned duplicate key");
+
+    // Get the tuple from the table
+    *tuple = table_info_->table_->GetTuple(rids[0]).second;
+    *rid = rids[0];
+
+    table_info_ = nullptr; // Reset the table info to avoid multiple scan
+
+    return true;
+
+}
 
 }  // namespace bustub
