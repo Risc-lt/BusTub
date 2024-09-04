56ef4b0c6f047a4d6b5b77edb59467b8790de20d
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-09-04 10:19:28 +0800

    feat: hash_join executor

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -11,21 +11,124 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/hash_join_executor.h"
+#include <cstddef>
+#include <cstdint>
+#include "type/value_factory.h"
 
 namespace bustub {
 
 HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&left_child,
                                    std::unique_ptr<AbstractExecutor> &&right_child)
-    : AbstractExecutor(exec_ctx) {
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      left_child_(std::move(left_child)),
+      right_child_(std::move(right_child)) {
   if (!(plan->GetJoinType() == JoinType::LEFT || plan->GetJoinType() == JoinType::INNER)) {
     // Note for 2023 Fall: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
   }
 }
 
-void HashJoinExecutor::Init() { throw NotImplementedException("HashJoinExecutor is not implemented"); }
+void HashJoinExecutor::Init() {
+  // Initialize the child executors
+  left_child_->Init();
+  right_child_->Init();
 
-auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+  // Initialize the left tuple
+  left_next_ = left_child_->Next(&left_tuple_, &left_rid_);
+
+  Tuple right_tuple;
+  RID right_rid;
+
+  // Initialize the hash table
+  jht_ = std::make_unique<SimpleHashJoinHashTable>();
+
+  // Build the hash table
+  while (right_child_->Next(&right_tuple, &right_rid)) {
+    jht_->InsertKey(GetRightJoinKey(&right_tuple), right_tuple);
+  }
+
+  // Get the matching right tuples for the first left tuple
+  auto left_key = GetLeftJoinKey(&left_tuple_);
+  right_tuples_ = jht_->GetValue(left_key);
+
+  // Initialize the iterator if right_tuples_ is not nullptr
+  if(right_tuples_ != nullptr) {
+    jht_iter_ = right_tuples_->begin();
+    // Set the flag
+    join_done_ = true;
+  } else {
+    join_done_ = false;
+  }
+}
+
+auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  // Continue the loop until we find a tuple that satisfies the join condition
+  while(true) {
+    //  If the right tuples are not nullptr and the iterator is not at the end
+    if(right_tuples_ != nullptr && jht_iter_ != right_tuples_->end()) {
+      std::vector<Value> values;
+      // Get the right tuple
+      Tuple right_tuple = *jht_iter_;
+
+      // Combine the left and right tuples
+      for (uint32_t i = 0; i < left_child_->GetOutputSchema().GetColumnCount(); i++) {
+        values.emplace_back(left_tuple_.GetValue(&left_child_->GetOutputSchema(), i));
+      }
+      for (uint32_t i = 0; i < right_child_->GetOutputSchema().GetColumnCount(); i++) {
+        values.emplace_back(right_tuple.GetValue(&right_child_->GetOutputSchema(), i));
+      }
+
+      // Create the tuple
+      *tuple = Tuple(values, &GetOutputSchema());
+      *rid = tuple->GetRid();
+
+      jht_iter_++;
+      return true;
+    }
+
+    // If the right tuples are nullptr or the iterator is at the end, 
+    // and it is left join and join_done_ shows that the left tuple has not matched
+    if (plan_->GetJoinType() == JoinType::LEFT && !join_done_) {    
+        std::vector<Value> values;
+        // Combine the left and right tuples
+        for (uint32_t i = 0; i < left_child_->GetOutputSchema().GetColumnCount(); i++) {
+          values.emplace_back(left_tuple_.GetValue(&left_child_->GetOutputSchema(), i));
+        }
+        for (uint32_t i = 0; i < right_child_->GetOutputSchema().GetColumnCount(); i++) {
+          values.emplace_back(ValueFactory::GetNullValueByType(right_child_->GetOutputSchema().GetColumn(i).GetType()));
+        }
+
+        // Create the tuple
+        *tuple = Tuple(values, &GetOutputSchema());
+        *rid = tuple->GetRid();
+
+        // Set the flag
+        join_done_ = true;
+        return true;
+    }
+
+    // If it is not left join or the left tuple has matched
+    // Get the next left tuple 
+    left_next_ = left_child_->Next(&left_tuple_, &left_rid_);
+    if (!left_next_) {
+      return false;
+    }
+
+    // Get the matching right tuples for the left tuple
+    auto left_key = GetLeftJoinKey(&left_tuple_);
+    right_tuples_ = jht_->GetValue(left_key);
+
+    // Initialize the iterator if right_tuples_ is not nullptr
+    if(right_tuples_ != nullptr) {
+      jht_iter_ = right_tuples_->begin();
+      // Set the flag
+      join_done_ = true;
+    } else {
+      join_done_ = false;
+    }
+  }
+}
 
 }  // namespace bustub
--- src/include/execution/executors/hash_join_executor.h
+++ src/include/execution/executors/hash_join_executor.h
@@ -15,10 +15,106 @@
 #include <memory>
 #include <utility>
 
+#include "common/util/hash_util.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
+#include "execution/executors/aggregation_executor.h"
 #include "execution/plans/hash_join_plan.h"
 #include "storage/table/tuple.h"
+
+/**
+  * Construct HashJoinKey with hash_keys_ member variable
+  */
+namespace bustub {
+
+/** HashJoinKey represents a key in an join operation */
+struct HashJoinKey {
+  std::vector<Value> hash_keys_;
+
+  /**
+   * Compares two hash joi keys for equality
+   * @param other the other hash join key to be compared with
+   * @return `true` if both hash join key have equivalent values
+   */
+  auto operator==(const HashJoinKey &other) const -> bool {
+    // Compare every hash key in the hash_keys_ vector
+    for (uint32_t i = 0; i < other.hash_keys_.size(); ++i) {
+      if (hash_keys_[i].CompareEquals(other.hash_keys_[i]) != CmpBool::CmpTrue) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+}  // namespace bustub
+
+/**
+  * Implement std::hash on AggregateKey
+  */
+namespace std {
+template <>
+struct hash<bustub::HashJoinKey> {
+  auto operator()(const bustub::HashJoinKey &join_key) const -> std::size_t {
+    size_t curr_hash = 0;
+    for (const auto &key : join_key.hash_keys_) {
+      if (!key.IsNull()) {
+        // Combine the hash value of the key with the current hash value
+        curr_hash = bustub::HashUtil::CombineHashes(curr_hash, bustub::HashUtil::HashValue(&key));
+      }
+    }
+    return curr_hash;
+  }
+};
+
+}  // namespace std
+
+/**
+  * Implement HashJoinExecutor
+  */
+namespace bustub {
+/**
+ * A simplified hash table that has all the necessary functionality for join.
+ */
+class SimpleHashJoinHashTable {
+ public:
+  /**
+    * Insert a key-value pair into the hash table.
+    * @param join_key The key to insert.
+    * @param tuple The value to insert.
+    */
+  void InsertKey(const HashJoinKey &join_key, const Tuple &tuple) {
+    if (ht_.count(join_key) == 0) {
+      std::vector<Tuple> tuple_vector;
+      tuple_vector.push_back(tuple);
+      ht_.insert({join_key, tuple_vector});
+    } else {
+      ht_.at(join_key).push_back(tuple);
+    }
+  }
+
+  /**
+   * Get the value associated with a key.
+   * @param join_key The key to look up.
+   * @return The value associated with the key.
+   */
+  auto GetValue(const HashJoinKey &join_key) -> std::vector<Tuple> * {
+    if (ht_.find(join_key) == ht_.end()) {
+      return nullptr;
+    }
+    return &(ht_.find(join_key)->second);
+  }
+
+  /**
+   * Clear the hash table
+   */
+  void Clear() { ht_.clear(); }
+
+ private:
+  /** The hash table is just a map from aggregate keys to aggregate values */
+  std::unordered_map<HashJoinKey, std::vector<Tuple>> ht_{};
+};
+
+}  // namespace bustub
 
 namespace bustub {
 
@@ -52,8 +148,58 @@ class HashJoinExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
  private:
+  /**
+   * Get the join key from the tuple for the left side of the join.
+    * @param tuple The tuple to get the join key from.
+    * @return The join key.
+   */
+  auto GetLeftJoinKey(const Tuple *tuple) -> HashJoinKey {
+    std::vector<Value> values;
+    for (const auto &expr : plan_->LeftJoinKeyExpressions()) {
+      values.emplace_back(expr->Evaluate(tuple, left_child_->GetOutputSchema()));
+    }
+    return {values};
+  }
+
+  /**
+   * Get the join key from the tuple for the right side of the join.
+   * @param tuple The tuple to get the join key from.
+   * @return The join key.
+   */
+  auto GetRightJoinKey(const Tuple *tuple) -> HashJoinKey {
+    std::vector<Value> values;
+    for (const auto &expr : plan_->RightJoinKeyExpressions()) {
+      values.emplace_back(expr->Evaluate(tuple, right_child_->GetOutputSchema()));
+    }
+    return {values};
+  }
+
   /** The HashJoin plan node to be executed. */
   const HashJoinPlanNode *plan_;
+
+  /** The hash table that stores the tuples from the left side of the join. */
+  std::unique_ptr<SimpleHashJoinHashTable> jht_;
+
+  /** Iterator for the hash table. */
+  std::vector<Tuple>::iterator jht_iter_;
+
+  /** The child executor that produces tuples for the left side of join. */
+  std::unique_ptr<AbstractExecutor> left_child_;
+
+  /** The child executor that produces tuples for the right side of join. */
+  std::unique_ptr<AbstractExecutor> right_child_;
+
+  /** The tuple from the left side of the join that is being processed. */
+  Tuple left_tuple_;
+  RID left_rid_;
+
+  /** The tuple from the right side of the join that is being processed. */
+  std::vector<Tuple> *right_tuples_{nullptr};
+
+  /**  Flags for join status. */
+  bool join_done_{false};
+  bool left_next_{false};
+
 };
 
 }  // namespace bustub
