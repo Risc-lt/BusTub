ebb76dc7136e2191938cac5bb5345439ef56e2de
Ruan Letian <1291903308rlt@sjtu.edu.cn>
2024-09-04 11:52:49 +0800

    feat: sort executor

--- src/execution/sort_executor.cpp
+++ src/execution/sort_executor.cpp
@@ -1,13 +1,44 @@
 #include "execution/executors/sort_executor.h"
+#include <utility>
 
 namespace bustub {
 
 SortExecutor::SortExecutor(ExecutorContext *exec_ctx, const SortPlanNode *plan,
                            std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void SortExecutor::Init() { throw NotImplementedException("SortExecutor is not implemented"); }
+void SortExecutor::Init() {
+    // Initialize the child executor
+    child_executor_->Init();
 
-auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    // Initialize the result set
+    Tuple tuple;
+    RID rid;
+
+    // Fetch all the tuples from the child executor
+    while (child_executor_->Next(&tuple, &rid)) {
+        tuples_.emplace_back(tuple);
+    }
+
+    // Get the sorting rule and sort the tuples
+    auto order_bys = plan_->GetOrderBy();
+    std::sort(tuples_.begin(), tuples_.end(), Comparator(&GetOutputSchema(), order_bys));
+
+    // Initialize the iterator
+    iter_ = tuples_.begin();
+}
+
+auto SortExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    // If the iterator reaches the end, return false
+    if (iter_ == tuples_.end()) {
+        return false;
+    }
+
+    // Fetch the tuple and increment the iterator
+    *tuple = *iter_;
+    ++iter_;
+
+    return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/sort_executor.h
+++ src/include/execution/executors/sort_executor.h
@@ -24,6 +24,51 @@
 namespace bustub {
 
 /**
+ * Comparator is a helper class that compares two tuples based on a set of order-bys.
+ */
+class Comparator {
+ public:
+  // Constructor 
+  Comparator() { schema_ = nullptr; }
+  Comparator(const Schema *schema, std::vector<std::pair<OrderByType, AbstractExpressionRef>> order_bys)
+      : schema_(schema), order_bys_(std::move(order_bys)) {}
+
+  auto operator()(const Tuple &t1, const Tuple &t2) -> bool {
+    for (auto const &order_by : this->order_bys_) {
+      const auto order_type = order_by.first;
+
+      // Get the value of the key
+      AbstractExpressionRef expr = order_by.second;
+      Value v1 = expr->Evaluate(&t1, *schema_);
+      Value v2 = expr->Evaluate(&t2, *schema_);
+
+      // Compare the two values based on the order type until find a difference
+      if (v1.CompareEquals(v2) == CmpBool::CmpTrue) {
+        continue;
+      }
+
+      // ASC or DEFAULT
+      if (order_type == OrderByType::ASC || order_type == OrderByType::DEFAULT) {
+        return v1.CompareLessThan(v2) == CmpBool::CmpTrue;
+      }
+      // DESC
+      return v1.CompareGreaterThan(v2) == CmpBool::CmpTrue;
+    }
+
+    // If all the keys are the same, return false
+    return false;
+  }
+
+ private:
+  /** The schema of the tuples to be compared */
+  const Schema *schema_;
+
+  /** The order-bys to sort the tuples */
+  std::vector<std::pair<OrderByType, AbstractExpressionRef>> order_bys_;
+};
+
+
+/**
  * The SortExecutor executor executes a sort.
  */
 class SortExecutor : public AbstractExecutor {
@@ -52,5 +97,12 @@ class SortExecutor : public AbstractExecutor {
  private:
   /** The sort plan node to be executed */
   const SortPlanNode *plan_;
+
+  /** The child executor used to fetch input tuples */
+  std::unique_ptr<AbstractExecutor> child_executor_;
+
+  /** The comparator used to sort the tuples */
+  std::vector<Tuple> tuples_;
+  std::vector<Tuple>::iterator iter_;
 };
 }  // namespace bustub
